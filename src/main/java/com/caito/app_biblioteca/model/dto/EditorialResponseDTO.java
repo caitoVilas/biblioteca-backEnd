7ae3e0c1458a8289a21fc4676be83eff63c1e8c8
claudio.vilas <caitocd@gmail.com>
2023-08-05 17:56:51 -0300

    editorial dtos created

---
+++ src/main/java/com/caito/app_biblioteca/model/dto/EditorialResponseDTO.java
@@ -0,0 +1,28 @@
+package com.caito.app_biblioteca.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+
+/**
+ * @author claudio.vilas
+ * date: 08/2023
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Schema(description = "modelo que representa una editorial para las respuesas")
+public class EditorialResponseDTO {
+    @Schema(name = "id")
+    private Long id;
+    @Schema(name = "name")
+    private String name;
+    @Schema(name = "state")
+    private String state;
+}
